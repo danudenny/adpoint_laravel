c20b4e14dcd7ed29f554d3200a91c01dde3be6a4
Denny <danudenny@gmail.com>
2020-01-21 11:03:44 +0700

    add pushy notification

--- public/js/enable-push.js
+++ public/js/enable-push.js
@@ -1,147 +1,73 @@
 'use strict';
 
-
-
 const swReady = navigator.serviceWorker.ready;
 
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-
     initSW();
-
 });
 
-
-
 function initSW() {
-
     if (!"serviceWorker" in navigator) {
-
-        //service worker isn't supported
-
         return;
-
     }
-
-
-
-    //don't use it here if you use service worker
-
-    //for other stuff.
 
     if (!"PushManager" in window) {
-
-        //push isn't supported
-
         return;
-
     }
 
-
-
-    //register the service worker
-
     navigator.serviceWorker.register('../sw.js')
-
     .then(() => {
-
         console.log('serviceWorker installed!')
-
         initPush();
-
     })
 
     .catch((err) => {
-
         console.log(err)
-
     });
 
 }
 
-
-
 function initPush() {
-
     if (!swReady) {
-
         return;
-
     }
 
-
-
     new Promise(function(resolve, reject) {
-
         const permissionResult = Notification.requestPermission(function(result) {
-
             resolve(result);
-
         });
 
-
-
         if (permissionResult) {
-
             permissionResult.then(resolve, reject);
-
         }
-
     })
 
     .then((permissionResult) => {
-
         if (permissionResult !== 'granted') {
-
             throw new Error('We weren\'t granted permission.');
-
         }
 
         subscribeUser();
-
     });
 
 }
 
-
-
-/**
-
- * Subscribe the user to push
-
- */
-
 function subscribeUser() {
-
     swReady
-
         .then((registration) => {
-
         const subscribeOptions = {
-
             userVisibleOnly: true,
-
             applicationServerKey: urlBase64ToUint8Array(
                 key
             )
-
         };
 
-
-
         return registration.pushManager.subscribe(subscribeOptions);
-
     })
 
     .then((pushSubscription) => {
-
         console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-
         storePushSubscription(pushSubscription);
-
     });
 
 }
