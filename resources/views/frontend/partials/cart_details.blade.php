5d64752b5f03b39301d8e94f2ba34e8be27a7d6b
alkhamil <alkhamilnaz@gmail.com>
2020-03-24 17:47:09 +0700

    Fixing bug cart details

--- resources/views/frontend/partials/cart_details.blade.php
+++ resources/views/frontend/partials/cart_details.blade.php
@@ -180,5 +180,46 @@
 
     <script type="text/javascript">
         cartQuantityInitialize();
-    </script>    
+    </script>
+@else 
+<div class="dc-header text-center p-5">
+    <h3 class="heading heading-6 strong-700">{{__('Your Cart is empty')}}</h3>
+    <div class="col mt-3 d-flex justify-content-center">
+        <img src="{{ url('img/not-found.png') }}" alt="">
+    </div>
+    <form method="post" action="{{URL::to('cart/adjust_budget')}}" >
+        <div class="col-md-8 mt-3" style="margin: 0 auto">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <span>{{ $error }}</span>
+                    @endforeach
+                </div>
+            @endif
+        </div>
+
+        <div class="col-md-8 mt-3" style="margin: 0 auto">
+            <label>
+                <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">Rp</span>
+                    </div>
+                    <input  name="budget" type="text" class="form-control" placeholder="Enter your budget" aria-label="Enter your budget" aria-describedby="basic-addon1">
+                </div>
+                @if(Session::has('budget'))
+                    <p class="mt-2">Your Budget is : </p>
+                    <h2>{{ single_price(Session::get('budget')) }}</h2>
+                @endif
+            </label>
+        </div>
+        <div style="margin-bottom: 30px">
+            <button type="submit" name="button" class="btn btn-lg btn-success"><i class="fa fa-money"></i> Adjust Your Budget</button>
+            @if(Session::has('budget'))
+                <a type="button" href="/cart/reset_budget" class="btn btn-lg btn-warning"><i class="fa fa-retweet"></i> Reset</a>
+            @endif
+        </div>
+    </form>
+</div>    
 @endif
+
