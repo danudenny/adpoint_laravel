23ab22a45831ac01b14ee4e284a32a24de8573be
Denny <danudenny@Gmail.com>
2020-04-09 21:03:37 +0700

    fix home page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
   {
     Schema::defaultStringLength(191);
       if (app()->environment('production')) {
-          $url::forceSchema('https');
+          $url->forceScheme('https');
       }
   }
 
